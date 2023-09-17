4ef9cb888a53fa9d159c7783b1c24758b600e763
Anzo <antonr@live.dk>
2023-09-17 22:42:28 +0200

    added libnepnep getter

---
+++ getLibNepNep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+BUILD_DIR=$1
+echo "Copying LibNepNep to ${BUILD_DIR}"
+rm -rf ./libNepNep
+rm -rf $BUILD_DIR/libNepNep
+git clone https://github.com/AnzoDK/HyperDimension-Neptunia-Save-Reasearch
+mv ./HyperDimension-Neptunia-Save-Reasearch/libNepNep ./libNepNep
+rm -rf ./HyperDimension-Neptunia-Save-Reasearch
+cd libNepNep
+make
+if [[ $? -ne 0 ]]
+then
+    echo "Failed to build LibNepNep"
+    exit 1
+fi
+mv ./libnepnep.* ../
+cd ..
+cp ./libnepnep.* $BUILD_DIR/
+cp -r ./libNepNep/include $BUILD_DIR/libNepNep
